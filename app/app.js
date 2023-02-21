23c6f239d09d32497f3bfd9ef097ca00eff47a9b
ferfcb99 <fer_fcb99@outlook.com>
2023-02-20 19:43:05 -0600

    Se sube carpeta

---
+++ app/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+    
+});
+
+const preguntas = [ 
+    ['¿Pregunta 1?', ['rpt1', false], ['rpt2', false], ['rpt3', true]],
+    ['¿Pregunta 2?', ['rpt1', false], ['rpt2', false], ['rpt3', true]],
+    ['¿Pregunta 3?', ['rpt1', false], ['rpt2', false], ['rpt3', true]],
+    ['¿Pregunta 4?', ['rpt1', false], ['rpt2', false], ['rpt3', true]],
+    ['¿Pregunta 5?', ['rpt1', false], ['rpt2', false], ['rpt3', true]],
+    ['¿Pregunta 6?', ['rpt1', false], ['rpt2', false], ['rpt3', true], ['rpt4', false]],
+];
+
+
+const pregunta = document.querySelector('.pregunta');
+// DOM - Document Object Model
