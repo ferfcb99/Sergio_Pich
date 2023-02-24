acd8f0b8caab40f79433f9757265b41e4fcc8220
ferfcb99 <fer_fcb99@outlook.com>
2023-02-23 21:02:09 -0600

    Se agrega funcion de boton siguiente

--- app/app.js
+++ app/app.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function(){
 // variables generales
 const rutaDeDatos = '../data/data.json';
 const preguntas = [];
+const examenes = [];
+let numeroPregunta = 0;
+let numeroExamen = 0;
 
 // botones
 const botonSiguiente = document.querySelector('.siguiente');
@@ -18,8 +21,11 @@ botonAtras.addEventListener('click', anteriorPregunta);
 
     
 function siguientePregunta(evt){
-    console.log("Se dio click");
-    console.log(evt);
+    numeroPregunta = numeroPregunta + 1;
+    const campoPregunta = document.querySelector('.preguntas');
+    const parrafo = document.querySelector('.parrafo');
+    campoPregunta.removeChild(parrafo);
+    getExamenPorId(rutaDeDatos, numeroExamen);
 }
 
 
@@ -27,20 +33,37 @@ function anteriorPregunta(evt){
     console.log("Evento atras")
 }
 
+function agregaPregunta(pregunta){
+    const campoPregunta = document.querySelector('.preguntas');
+    const parrafo = document.createElement('p');
+    parrafo.textContent = pregunta['pregunta'];
+    parrafo.classList.add('parrafo');
+    campoPregunta.appendChild(parrafo);
+    console.log(parrafo);
+}
+
+
 
 function iteraExamen(examen){
     console.log(examen['noExamen']);
     console.log(examen['nombreExamen']);
-    console.log("Entrando al foreach")
-    examen['preguntas'].forEach( (pregunta) =>{
-        /*
-        console.log("----------------")
-        console.log(pregunta['noPregunta'])
-        console.log(pregunta['pregunta'])
-        console.log(pregunta['imagen'])
-        */
-        preguntas.push(pregunta);
-    });
+
+    for (let i = 0; i < examen['preguntas'].length; i++) {
+        if(i == numeroPregunta){
+            const pregunta = examen['preguntas'][i];
+            agregaPregunta(pregunta);
+        }
+    }
+    /*examen['preguntas'].forEach( (pregunta, index) =>{
+        // preguntas.push(pregunta);
+        // console.log(pregunta['pregunta']);
+        if(index === numeroPregunta){
+            console.log(pregunta['pregunta']);
+            console.log("Saliendo")
+            return;
+        }
+        agregaPregunta(pregunta);
+    });*/
 }
 
 // funcion que consulta los datos de una ruta
@@ -56,9 +79,13 @@ function getExamenPorId(ruta, noExamen){
         });
 }
 
-getExamenPorId(rutaDeDatos, 2);
+getExamenPorId(rutaDeDatos, numeroExamen);
+// sconsole.log(preguntas)
 
-console.log(preguntas);
+
+
+
+
 
 /*
 const numeros = [2.3,23,435,5,2,-7,32];
